0b3812d7a126a8ef985b4a48be167f60f2122f2f
rachfuerst <rach.fuerst@gmail.com>
2020-01-31 07:02:04 -0600

    Installed Semantic and implemented grid for login container

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from "./LoginContainer"
 
 class App extends React.Component {
 
   
 
   componentDidMount(){
-    fetch('http://localhost:4000/profiles/1', {
-      method: 'GET',
+    // fetch('http://localhost:4000/profiles/1', {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Accept' : 'application/json'
+    //   }
+    // })
+    // .then(response => response.json())
+    // .then(data => this.setState({profile: data.profile, avatar: data.avatar}))
+
+    fetch("http://localhost:4000/users", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept' : 'application/json'
-      }
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        user: {
+          username: "lisa2",
+          password: "hi",
+          email: "b@gmail.com"
+        }
+      })
     })
-    .then(response => response.json())
-    .then(data => this.setState({profile: data.profile, avatar: data.avatar}))
+      .then(r => r.json())
+      .then(console.log);
   }
+  
 
   render() {
     return (
       <div>
-      
+        <Login />
         {/* <img src={`http://localhost:4000/${this.state.avatar}`}/> */}
       </div>
     )
---
+++ src/LoginContainer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Grid, Container, Segment } from "semantic-ui-react";
+
+export default function Login() {
+    return (
+        <Container>
+            <Segment>
+                <Grid>
+                <Grid.Row>
+              <Grid.Column width={8}>
+               
+              </Grid.Column>
+              <Grid.Column width={8}>
+
+              </Grid.Column>
+            </Grid.Row>
+                </Grid>
+            </Segment>
+        </Container>
+    )
+}
