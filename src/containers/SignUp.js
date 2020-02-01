5d317b50b292a754d0081d424ba944ee1ffb372c
rachfuerst <rach.fuerst@gmail.com>
2020-02-01 10:19:29 -0600

    Adding ability to store the jwt token in localStorage

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -71,7 +69,10 @@ export default function SignUp() {
         })
       })
         .then(r => r.json())
-        .then(console.log);
+        .then(response => {
+            console.log(response);
+            localStorage.setItem("jwt", response.jwt);
+          });
     }
 
 
@@ -85,7 +86,7 @@ export default function SignUp() {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <form className={classes.form} noValidate onSubmit={event => handleSubmit(event)}>
+        <form className={classes.form}  onSubmit={event => handleSubmit(event)}>
           <Grid container spacing={2}>
             <Grid item xs={12} >
               <TextField
