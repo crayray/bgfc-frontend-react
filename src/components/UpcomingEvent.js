7af7ee1d304d68ad2a5dcd41dd25c7714fde983d
rachfuerst <rach.fuerst@gmail.com>
2020-02-02 11:56:15 -0600

    Successfully getting stock events into cards. More card styling to come.

--- src/components/UpcomingEvent.js
+++ src/components/UpcomingEvent.js
@@ -1,28 +1,26 @@
 import React from "react";
 import { Button, Card, Image } from "semantic-ui-react";
+import { defaultProps } from "grommet";
 
-export default function UpcomingEvent() {
+export default function UpcomingEvent({imageName}) {
   return (
     <Card>
       <Card.Content>
         <Image
-          floated="right"
-          size="mini"
-          src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+          floated="center"
+          // size="mini"
+          src= {imageName}
         />
-        <Card.Header>Steve Sanders</Card.Header>
-        <Card.Meta>Friends of Elliot</Card.Meta>
-        <Card.Description>
+        {/* <Card.Header>Steve Sanders</Card.Header>
+        <Card.Meta>Friends of Elliot</Card.Meta> */}
+        {/* <Card.Description>
           Steve wants to add you to the group <strong>best friends</strong>
-        </Card.Description>
+        </Card.Description> */}
       </Card.Content>
       <Card.Content extra>
         <div className="ui two buttons">
-          <Button basic color="green">
-            Approve
-          </Button>
-          <Button basic color="red">
-            Decline
+          <Button inverted color="olive">
+            RSVP
           </Button>
         </div>
       </Card.Content>
--- src/containers/EventsLayout.js
+++ src/containers/EventsLayout.js
@@ -4,12 +4,20 @@ import { Container, Segment } from "semantic-ui-react"
 import NavBar from "../components/NavBar"
 
 export default class EventsLayout extends Component {
+
+
     render() {
+
+        const eventImages = ['http://localhost:3000/january_meetup.png', 'http://localhost:3000/december_meetup.png','http://localhost:3000/bgfc-logo.svg' ]
         return (
            <div>
            <NavBar />
             <Segment>
-               <UpcomingEvent />
+            {eventImages.map(image => (
+                <UpcomingEvent 
+                    imageName={image}
+                />
+            ))}
                </Segment>
                </div>
         )
