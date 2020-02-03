ba08a794625430b8b96280b5116137e0b4ef4280
rachfuerst <rach.fuerst@gmail.com>
2020-02-03 07:10:29 -0600

    Putting the Link around buttons in RSVP/event Modal

--- src/components/RsvpModal.js
+++ src/components/RsvpModal.js
@@ -1,39 +1,41 @@
-import React from 'react'
-import { Button, Header, Image, Modal, Icon } from 'semantic-ui-react'
+import React from "react";
+import { Button, Header, Image, Modal, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-
-
 const ModalModalExample = () => (
-  <Modal trigger={<Button inverted color="olive">RSVP</Button>}>
+  <Modal
+    trigger={
+      <Button inverted color="olive">
+        RSVP
+      </Button>
+    }
+  >
     <Modal.Header>Event Details</Modal.Header>
     <Modal.Content image>
-      <Image wrapped size='small' src='http://localhost:3000/woman_logo.png' />
+      <Image wrapped size="small" src="http://localhost:3000/woman_logo.png" />
       <Modal.Description>
         <Header>Casa Columbia (Event Name)</Header>
-        <p>
-         Time
-         Date
-         Address
-        </p>
+        <p>Time Date Address</p>
       </Modal.Description>
     </Modal.Content>
     <Modal.Actions>
-    <Link to="/signup">
-    <Button color="olive" icon labelPosition='left'>
-      <Icon name='close' />
-       Signup 
-    </Button>
-    </Link>
-    <Link to="/login"> 
-     <Button color="olive"  icon labelPosition='right'>
-          Login<Icon name='right chevron' />
-          </Button></Link> 
+      <Link to="/signup">
+        <Button color="olive" icon labelPosition="left">
+          <Icon name="close" />
+          Signup
+        </Button>
+      </Link>
+      <Link to="/login">
+        <Button color="olive" icon labelPosition="right">
+          Login
+          <Icon name="right chevron" />
+        </Button>
+      </Link>
     </Modal.Actions>
     {/* <Modal.Actions>
       <NestedModal />
     </Modal.Actions> */}
   </Modal>
-)
+);
 
-export default ModalModalExample
+export default ModalModalExample;
