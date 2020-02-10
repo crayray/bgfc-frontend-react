85945677e373125b314644a1e2e1c6ab3766c232
rachfuerst <rach.fuerst@gmail.com>
2020-02-10 11:34:22 -0600

    Successfully getting layout for Create profile form

--- src/components/CreateProfileForm.js
+++ src/components/CreateProfileForm.js
@@ -7,9 +7,10 @@ import {
   Header
 } from "semantic-ui-react";
 import { DirectUpload } from "activestorage";
-import OopsProfile from "./OopsProfile"
+import OopsProfile from "./OopsProfile";
 
 import React, { Component } from "react";
+import "../stylesheets/CreateProfile.css";
 
 const options = [
   { key: "a", text: "Activism", value: "Activism" },
@@ -119,102 +120,122 @@ export default class CreateProfileForm extends Component {
     });
   };
   render() {
-
     if (this.state.profile === null) {
-    return (
-      <Container style={{ marginTop: "75px" }}>
-        <Grid
-          textAlign="center"
-          style={{ height: "100vh" }}
-          verticalAlign="middle"
-        >
-          <Grid.Column style={{ maxWidth: 450 }}>
-            <Form size="large" onSubmit={this.handleSubmit}>
-              <Segment>
-                <Form.Input
-                  icon="user circle olive"
-                  name="name"
-                  placeholder="Your name here"
-                  value={this.state.name}
-                  onChange={this.handleOnChange}
-                  type="text"
-                  label="Name"
-                />
-
-                <Form.Input
-                  icon="quote right olive"
-                  name="about"
-                  label="Share a short blurb about yourself"
-                  placeholder="A blurb about me...."
-                  value={this.state.about}
-                  onChange={this.handleOnChange}
-                />
-              </Segment>
-              <Segment>
-                <Header>
-                  Share your interests and Social media details so other members
-                  can find you:
-                </Header>
-                <Form.Select
-                  fluid
-                  label="Select an Interest"
-                  name="interest"
-                  options={options}
-                  placeholder="Select an Interest"
-                  onChange={(e, { value, text }) => {
-                    this.setState({
-                      interest1: value
-                    });
-                  }}
-                />
-
-                <Form.Input
-                  icon="instagram olive"
-                  name="instagram"
-                  placeholder="Your Instagram handle"
-                  value={this.state.instagram}
-                  onChange={this.handleOnChange}
-                  type="text"
-                  label="Instagram"
-                />
-
-                <Form.Input
-                  icon="twitter olive"
-                  name="twitter"
-                  placeholder="Your Twitter handle"
-                  value={this.state.twitter}
-                  onChange={this.handleOnChange}
-                  type="text"
-                  label="Twitter"
-                />
-
-                <Form.Input
-                  icon="facebook olive"
-                  name="facebook"
-                  placeholder="Your Facebook name"
-                  value={this.state.facebook}
-                  onChange={this.handleOnChange}
-                  type="text"
-                  label="Facebook"
-                />
-              </Segment>
-              <Segment>
-                <Form.Field>
-                  <label>Upload your Profile photo:</label>
-                  <input
-                    type="file"
-                    name="avatar"
+      return (
+        <Container className="form-container">
+          <Grid
+            textAlign="center"
+            // style={{ height: "100vh" }}
+            // verticalAlign="middle"
+          >
+            <Grid.Column style={{ maxWidth: 1000 }}>
+              <Form onSubmit={this.handleSubmit}>
+                <Segment className="segment-padding">
+                  <Header as="h3">Name</Header>
+                  <Form.Input
+                    icon="user circle olive"
+                    name="name"
+                    placeholder="Your name here"
+                    value={this.state.name}
                     onChange={this.handleOnChange}
+                    type="text"
+                    size="big"
+                    className="input-margins"
                   />
-                </Form.Field>
-                <Form.Field control={Button}>Create my profile</Form.Field>
-              </Segment>
-            </Form>
-          </Grid.Column>
-        </Grid>
-      </Container>
-    )}else{
-      return (<OopsProfile />)
+                  <Header as="h3">Share a short blurb about yourself</Header>
+                  <Form.TextArea
+                    icon="quote right olive"
+                    name="about"
+                    placeholder="A blurb about me...."
+                    value={this.state.about}
+                    onChange={this.handleOnChange}
+                    size="big"
+                    className="input-margins"
+                  />
+                </Segment>
+                <Segment className="section segment-padding">
+                  <Header as="h2" className="text">
+                    {" "}
+                    To help other members connect with you, please share your
+                    social media details and select an interest.
+                  </Header>
+                  <Header as="h3">Select one interest:</Header>
+                  <Form.Select
+                    fluid
+                    name="interest"
+                    options={options}
+                    size="big"
+                    placeholder="Select an Interest"
+                    onChange={(e, { value, text }) => {
+                      this.setState({
+                        interest1: value
+                      });
+                    }}
+                    className="input-margins"
+                  />
+                  <Header as="h3">Instagram</Header>
+                  <Form.Input
+                    icon="instagram olive"
+                    name="instagram"
+                    placeholder="Your Instagram handle"
+                    value={this.state.instagram}
+                    onChange={this.handleOnChange}
+                    type="text"
+                    size="big"
+                    className="input-margins"
+                  />
+                  <Header as='h3'>Twitter</Header>
+                  <Form.Input
+                    icon="twitter olive"
+                    name="twitter"
+                    placeholder="Your Twitter handle"
+                    value={this.state.twitter}
+                    onChange={this.handleOnChange}
+                    type="text"
+                    
+                    size="big"
+                    className="input-margins"
+                  />
+                  <Header as='h3'>Facebook</Header>
+                  <Form.Input
+                    icon="facebook olive"
+                    name="facebook"
+                    placeholder="Your Facebook profile URL"
+                    value={this.state.facebook}
+                    onChange={this.handleOnChange}
+                    type="text"
+                    // label="Facebook"
+                    size="big"
+                    className="input-margins"
+                  />
+                </Segment>
+                <Segment className="section segment-padding">
+                  <Form.Field>
+                  <Header as='h3'>Upload your Profile photo:</Header>
+                    {/* <label>Upload your Profile photo:</label> */}
+                    <input
+                      type="file"
+                      name="avatar"
+                      onChange={this.handleOnChange}
+                      size="big"
+                      className="input-margins"
+                    />
+                  </Form.Field>
+                  <Form.Field
+                    className="input-margins"
+                    size="medium"
+                    control={Button}
+                  >
+                    Create my profile
+                  </Form.Field>
+                </Segment>
+              </Form>
+            </Grid.Column>
+          </Grid>
+        </Container>
+      );
+    } else {
+      return <OopsProfile />;
     }
   }
 }
