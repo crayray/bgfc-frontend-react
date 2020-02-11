53af331a7ace416445ca15fcf18bb303efdef349
rachfuerst <rach.fuerst@gmail.com>
2020-02-11 08:09:33 -0600

    Members Layout Styling

--- src/containers/MembersLayout.js
+++ src/containers/MembersLayout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import { Container, Segment, Grid, Header } from "semantic-ui-react";
+import { Container, Segment, Grid, Header, Image } from "semantic-ui-react";
 import { events } from "../data/events";
 import ProfileCardFront from "../components/ProfileCardFront";
 import CreateProfileForm from "../components/CreateProfileForm";
 import SearchBar from "../components/SearchBar";
 import CardContainer from "./CardContainer";
+import "../stylesheets/MembersLayout.css";
 
 export default class MembersLayout extends React.Component {
   state = {
@@ -43,23 +44,47 @@ export default class MembersLayout extends React.Component {
   };
 
   render() {
-    const {searchField, profiles, interestFilter} = this.state;
+    const { searchField, profiles, interestFilter } = this.state;
     const isInSearchField = profile => {
-      return profile.name
-        .toLowerCase()
-        .includes(searchField.toLowerCase());
-    }
+      return profile.name.toLowerCase().includes(searchField.toLowerCase());
+    };
     const namefilteredProfiles = profiles.filter(profile => {
-      return isInSearchField(profile) && profile.interest1.toLowerCase().includes(interestFilter.toLowerCase())
+      return (
+        isInSearchField(profile) &&
+        profile.interest1.toLowerCase().includes(interestFilter.toLowerCase())
+      );
     });
 
-     
     return (
-      <div>
-        <SearchBar
-          onSearchChange={this.onSearchChange}
-          onDropdownChange={this.onDropdownChange}
-        />
+      <Container className="members-container">
+        <Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Header textAlign="center" as="h1">
+                Connect with other BGFC Members
+              </Header>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            {/* style={{ maxWidth: 700 }} */}
+            <Grid.Column width={8}>
+              <Header textAlign="center" as="h4" >Looking for someone you met at one of our meetups?</Header>
+            </Grid.Column>
+            <Grid.Column width={8}>
+            <Image
+                centered
+                size="tiny"
+                src="http://localhost:3000/logos/bgfc-logo-neon.svg"
+                style={{ borderRadius: "50%" }}
+              />
+              <SearchBar
+                onSearchChange={this.onSearchChange}
+                onDropdownChange={this.onDropdownChange}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
         <Segment style={{ padding: "8em 0em" }} vertical>
           <Grid container>
             <Grid.Row>
@@ -70,7 +95,7 @@ export default class MembersLayout extends React.Component {
           </Grid>
         </Segment>
         <CardContainer profiles={namefilteredProfiles} />
-      </div>
+      </Container>
     );
   }
 }
