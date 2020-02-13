ae4ac6401c8eee9b66e826ba448898b2366c0a99
rachfuerst <rach.fuerst@gmail.com>
2020-02-13 10:26:21 -0600

    styling map restaurant card

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { Card, Popup, Header } from "semantic-ui-react";
+import { Card, Popup, Header, Label } from "semantic-ui-react";
 
 export default function RestaurantCard({ name, desc, cuisine, yelp }) {
   return (
     <Popup
-     position='left center'
-    trigger={
-    <Card href={yelp}>
-      <Card.Content>
-        <Card.Header>{name}</Card.Header>
-      </Card.Content>
-      <Card.Content>
-        {desc}
-        {cuisine}
-      </Card.Content>
-    </Card> }
+      position="left center"
+      trigger={
+        <Card href={yelp}>
+          <Card.Content>
+            <Card.Header>{name}</Card.Header>
+          </Card.Content>
+          <Card.Content>
+            <Header as="h4">{desc}</Header>
+
+            <Label as="a" color="teal" image>
+              {cuisine}
+            </Label>
+          </Card.Content>
+        </Card>
+      }
     >
-           <Popup.Content >Click me to see {name}'s' Yelp page!</Popup.Content>
- 
+      <Popup.Content>Click me to see {name}'s' Yelp page!</Popup.Content>
     </Popup>
   );
 }
